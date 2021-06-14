3c2211c6ebf40992e03412deb0ff7bcaa05eb3e7
Hoang Van Chung <hoang@hoang-HP-ProBook-640-G1chunghv.tny@gmail.com>
2021-06-14 14:24:28 +0700

    ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FontSizeEditorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
